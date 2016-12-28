180993a70181e7b0b790bc31665b43cabe2c7357
DeKKeRGOD <tigrenvova@mail.ru>
2016-12-29 02:36:11 +0300

    Update Lab_3.cpp

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -2,34 +2,25 @@
 #include <stdlib.h>
 void main()
 {
-double *pMas; // Указатель на массив
-int n; // Заранее неизвестная размерность массива
-// Вводим размерность массива с клавиатуры
+double *aMas;
+int n; // Вводим размерность массива с клавиатуры
 printf_s("n=");
 scanf_s("%d", &n);
-// Выделяем память динамически
-pMas = (double *)malloc(n*sizeof(double));
-// Заполняем массив случайными числами в интервале [0, 100)
-// и печатаем число
+aMas = (double *)malloc(n*sizeof(double));
 for (int i = 0; i<n; i++)
 {
-pMas[i] = rand() % 100;
-printf("%f ", pMas[i]);
+aMas[i] = rand() % 100; // Заполняем массив случайными числами в интервале от 0 до 99
+printf("%f ", aMas[i]);
 }
-// Переворачиваем массив
 for (int i = 0; i<n / 2; i++)
 {
-// Меняем 2 элемента местами, используя буферную переменную
-double buf = pMas[i];
-pMas[i] = pMas[n - 1 - i];
-pMas[n - 1 - i] = buf;
+double buf = aMas[i];
+aMas[i] = aMas[n - 1 - i];
+aMas[n - 1 - i] = buf;
 }
-// Печатаем полученный массив
 printf("\n");
 for (int i = 0; i<n; i++)
-printf("%f ", pMas[i]);
-// Освобождаем память
-free(pMas);
-// Останавливаем программу, ждем нажатия любой клавиши
-system("pause");// Останавливаем программу, ждем нажатия любой клавиши
+printf("%f ", aMas[i]);
+free(aMas);
+system("pause");
 }
